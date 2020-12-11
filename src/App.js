82832a819152e6d092a881f329fac1461c9d9286
mma judith <mmajudith2000@gmail.com>
2020-12-10 17:45:51 -0800

    Room Furnitures

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.scss';
 import Header from './components/Header';
 import About from './components/About';
-import Attribution from './components/Attribution';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <About />
-      <Attribution />
     </div>
   );
 }
