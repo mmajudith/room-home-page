cd318b2eb12fd24f56fa40ef867e2f07a737d51c
mma judith <mmajudith2000@gmail.com>
2020-12-10 17:07:11 -0800

    Updates

---
+++ precache-manifest.ed62c8fecb27c47ac1730515bb104bae.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370633a31c43e172bfbf59c25b4872ee",
+    "url": "/room-home-page/index.html"
+  },
+  {
+    "revision": "a42a6f37cd42b3960962",
+    "url": "/room-home-page/static/css/main.46c006b3.chunk.css"
+  },
+  {
+    "revision": "d08130a269f4536c9142",
+    "url": "/room-home-page/static/js/2.a340796c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/room-home-page/static/js/2.a340796c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a42a6f37cd42b3960962",
+    "url": "/room-home-page/static/js/main.101abaf8.chunk.js"
+  },
+  {
+    "revision": "cb6be93affca15991c11",
+    "url": "/room-home-page/static/js/runtime-main.687655d9.js"
+  },
+  {
+    "revision": "013946438a56b8adb0f8a258744097b6",
+    "url": "/room-home-page/static/media/desktop-image-hero-1.01394643.jpg"
+  },
+  {
+    "revision": "03660ebe1a314460126a9cb74b13aae1",
+    "url": "/room-home-page/static/media/desktop-image-hero-2.03660ebe.jpg"
+  },
+  {
+    "revision": "63998c1c2f52977e90efcabe0da730ed",
+    "url": "/room-home-page/static/media/desktop-image-hero-3.63998c1c.jpg"
+  },
+  {
+    "revision": "09c8dfd5db5a83a529a50737eeae21f9",
+    "url": "/room-home-page/static/media/icon-angle-left.09c8dfd5.svg"
+  },
+  {
+    "revision": "2f95dbcbb0d41f53d9e6d78ecfdd2d9a",
+    "url": "/room-home-page/static/media/icon-angle-right.2f95dbcb.svg"
+  },
+  {
+    "revision": "413b6204ca5190bea91724a3be2ad2f8",
+    "url": "/room-home-page/static/media/icon-arrow.413b6204.svg"
+  },
+  {
+    "revision": "2992ccac683d5dac7600a04d23abf702",
+    "url": "/room-home-page/static/media/icon-close.2992ccac.svg"
+  },
+  {
+    "revision": "0b39db49029721522332d26ba10b2737",
+    "url": "/room-home-page/static/media/icon-hamburger.0b39db49.svg"
+  },
+  {
+    "revision": "fa292a671bd24f028df716cb0b106381",
+    "url": "/room-home-page/static/media/image-about-dark.fa292a67.jpg"
+  },
+  {
+    "revision": "11c1e8e670af79117d6ade5868882f59",
+    "url": "/room-home-page/static/media/logo.11c1e8e6.svg"
+  },
+  {
+    "revision": "6ee5d72deab99ccf2c8fb4f9af9237ba",
+    "url": "/room-home-page/static/media/mobile-image-hero-1.6ee5d72d.jpg"
+  },
+  {
+    "revision": "d77b8410fc08b023b94d1fe9d1133109",
+    "url": "/room-home-page/static/media/mobile-image-hero-2.d77b8410.jpg"
+  },
+  {
+    "revision": "39ee9deba6f21e5286ce56012e444d05",
+    "url": "/room-home-page/static/media/mobile-image-hero-3.39ee9deb.jpg"
+  }
+]);
